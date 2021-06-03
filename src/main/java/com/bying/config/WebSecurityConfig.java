166fc7317b135c49af24dcb3f4ea5e81681134b9
chengkunxf <chengkunxf@126.com>
2021-06-03 15:46:11 +0800

    init

---
+++ src/main/java/com/bying/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.bying.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+/**
+ * @author chengkunxf@126.com
+ * @date 2021/6/3 2:47 下午
+ * @description
+ */
+
+@EnableWebSecurity
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);//加这句是为了访问eureka控制台和/actuator时能做安全控制
+        http.csrf().disable();//关闭csrf
+    }
+
+}
